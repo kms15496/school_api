ed63fc3c73c58aa90971274670093367b521a4d5
kaungmyatsankyaw <kaungmyatsan154996@gmail.com>
2026-05-24 01:10:51 +0700

    feat: icon url fix

--- app/Http/Controllers/API/ConfigController.php
+++ app/Http/Controllers/API/ConfigController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ConfigController extends Controller
 {
@@ -15,49 +15,49 @@ class ConfigController extends Controller
                     'key' => 'lesson_plan',
                     'enabled' => true,
                     'order' => 1,
-                    'image' => asset('app_icons/icon_lesson.png'),
+                    'image' => url('/api/app-icons/icon_lesson.png'),
                 ],
                 [
                     'key' => 'school_fees',
                     'enabled' => true,
                     'order' => 2,
-                    'image' => asset('app_icons/icon_fees.png'),
+                    'image' => url('/api/app-icons/icon_fees.png'),
                 ],
                 [
                     'key' => 'attendance',
                     'enabled' => true,
                     'order' => 3,
-                    'image' => asset('app_icons/icon_attendance.png'),
+                    'image' => url('/api/app-icons/icon_attendance.png'),
                 ],
                 [
                     'key' => 'exam_timetable',
                     'enabled' => true,
                     'order' => 4,
-                    'image' => asset('app_icons/icon_exam.png'),
+                    'image' => url('/api/app-icons/icon_exam.png'),
                 ],
                 [
                     'key' => 'report_card',
                     'enabled' => true,
                     'order' => 5,
-                    'image' => asset('app_icons/icon_result.png'),
+                    'image' => url('/api/app-icons/icon_result.png'),
                 ],
                 [
                     'key' => 'announcements',
                     'enabled' => true,
                     'order' => 6,
-                    'image' => asset('app_icons/icon_information.png'),
+                    'image' => url('/api/app-icons/icon_information.png'),
                 ],
                 [
                     'key' => 'school_events',
                     'enabled' => true,
                     'order' => 7,
-                    'image' => asset('app_icons/icon_events.png'),
+                    'image' => url('/api/app-icons/icon_events.png'),
                 ],
                 [
                     'key' => 'school_info',
                     'enabled' => false,
                     'order' => 8,
-                    'image' => asset('app_icons/icon_school.png'),
+                    'image' => url('/api/app-icons/icon_school.png'),
                 ],
             ],
         ];
@@ -66,6 +66,20 @@ class ConfigController extends Controller
             'status' => true,
             'message' => 'Config retrieved successfully',
             'data' => $config,
+        ]);
+    }
+
+    public function getAppIcon(string $filename)
+    {
+        abort_unless(preg_match('/\Aicon_[a-z_]+\.png\z/', $filename), 404);
+
+        $path = public_path('app_icons/' . $filename);
+
+        abort_unless(File::isFile($path), 404);
+
+        return response()->file($path, [
+            'Content-Type' => 'image/png',
+            'Cache-Control' => 'public, max-age=31536000',
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -62,4 +62,4 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 });
 
 Route::get('/config', [\App\Http\Controllers\API\ConfigController::class, 'getConfig']);
-
+Route::get('/app-icons/{filename}', [\App\Http\Controllers\API\ConfigController::class, 'getAppIcon']);
